896576fc69626e8798789e78645b6635f97d94f9
yoon052 <105267838+yoon052@users.noreply.github.com>
2023-08-08 22:24:46 +0900

    feat: basic layout
    Position Header and Footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Open_Sans } from 'next/font/google';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 const sans = Open_Sans({ subsets: ['latin'] });
 
@@ -16,7 +18,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en' className={sans.className}>
-      <body>{children}</body>
+      <body>
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </body>
     </html>
   );
 }
