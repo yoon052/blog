4340ec5214223b8f80cb484f1f6b918a6e1a7f3f
yoon052 <105267838+yoon052@users.noreply.github.com>
2023-08-15 20:15:47 +0900

    feat: basic blog frame

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,9 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en' className={sans.className}>
-      <body>
+      <body className='flex flex-col w-full max-w-screen-2xl mx-auto'>
         <Header />
-        <main>{children}</main>
+        <main className='grow'>{children}</main>
         <Footer />
       </body>
     </html>
