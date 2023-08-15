7d16b021c5fcf0fb408bcedadf92fbbd8f78e5c2
yoon052 <105267838+yoon052@users.noreply.github.com>
2023-08-15 20:54:44 +0900

    feat: FeaturePosts
    use FeaturePosts component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from '@/components/Hero';
+import FeaturedPosts from '@/components/FeaturedPosts';
 import Image from 'next/image';
 
 export default function HomePage() {
   return (
     <>
       <Hero />
+      <FeaturedPosts />
     </>
   );
 }
