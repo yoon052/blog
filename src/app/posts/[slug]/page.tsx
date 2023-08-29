5f9df426876be841250ca90bb231167c947b8ada
yoon052 <105267838+yoon052@users.noreply.github.com>
2023-08-29 20:52:50 +0900

    feat: markdown
    use calendar icons from react-icons

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,16 +1,17 @@
 import MarkdownViewer from '@/components/MarkdownViewer';
+import PostContent from '@/components/PostContent';
 import { getPostData } from '@/service/posts';
 import Image from 'next/image';
-import { AiTwotoneCalendar } from 'react-icons/ai';
 
 type Props = {
   params: {
     slug: string;
   };
 };
-
 export default async function PostPage({ params: { slug } }: Props) {
-  const { title, description, date, path, content } = await getPostData(slug);
+  const post = await getPostData(slug);
+  const { title, path } = post;
+
   return (
     <article className='rounded-2xl overflow-hidden bg-gray-100 shadow-lg m-4'>
       <Image
@@ -20,16 +21,7 @@ export default async function PostPage({ params: { slug } }: Props) {
         width={760}
         height={420}
       />
-      <section className='flex flex-col p-4'>
-        <div className='flex items-center self-end text-sky-600'>
-          <AiTwotoneCalendar />
-          <p className='font-semibold ml-2'>{date.toString()}</p>
-        </div>
-        <h1 className='text-4xl font-bold'>{title}</h1>
-        <p className='text-xl font-bold'>{description}</p>
-        <div className='w-44 border-2 border-sky-600 mt-4 mb-8'></div>
-        <MarkdownViewer content={content} />
-      </section>
+      <PostContent post={post} />
     </article>
   );
 }
